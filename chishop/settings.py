1b4f4fbe4a15da827e01c7c1811bcec9418bd027
Phil Christensen <pchristensen@freelancersunion.org>
2011-08-10 15:29:38 -0400

    removed remote_user support

--- chishop/settings.py
+++ chishop/settings.py
@@ -18,18 +18,6 @@ DATABASES = dict(
 	)
 )
 
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.RemoteUserMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
-
-AUTHENTICATION_BACKENDS = (
-    'django.contrib.auth.backends.RemoteUserBackend',
-)
-
 HAYSTACK_SEARCH_ENGINE = 'whoosh'
 HAYSTACK_SITECONF = 'chishop.search_sites'
 HAYSTACK_WHOOSH_PATH = os.path.join(os.path.dirname(chishop.__file__), 'haystack')
