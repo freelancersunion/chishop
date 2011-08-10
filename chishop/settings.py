207a65ea1b7f29384ee51f4fc00e09c075ec2e0b
Phil Christensen <pchristensen@freelancersunion.org>
2011-08-10 15:25:01 -0400

    enabling basic auth

--- chishop/settings.py
+++ chishop/settings.py
@@ -5,6 +5,8 @@ from chishop.conf.default import *
 DEBUG = True
 TEMPLATE_DEBUG = True
 
+TIME_ZONE = 'America/New_York'
+
 DATABASES = dict(
 	default = dict(
 		ENGINE = 'django.db.backends.postgresql_psycopg2',
@@ -14,6 +16,18 @@ DATABASES = dict(
 		HOST = '',
 		PORT = '',
 	)
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.auth.middleware.RemoteUserMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.RemoteUserBackend',
 )
 
 HAYSTACK_SEARCH_ENGINE = 'whoosh'
